c908733b1ad6a0733a2659066280770087aeaa0a
GastonInn <gaston.i.0201@gmail.com>
2024-05-13 13:04:02 -0300

    Adding changes to CORS

--- ms-commerce/src/main/java/com/mscommerce/MsCommerceApplication.java
+++ ms-commerce/src/main/java/com/mscommerce/MsCommerceApplication.java
@@ -3,6 +3,9 @@ package com.mscommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class MsCommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsCommerceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
